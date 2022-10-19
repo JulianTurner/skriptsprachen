5b2f9fe9c9713b8430eade26ac9d26f17c420884
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2022-10-19 18:37:32 +0000

    fixed spaces

--- dunder.py
+++ dunder.py
@@ -1,26 +1,26 @@
 # Klasse welche die dunder Methode enthält
 class Datenhalter:
-	def __init__(self, wert):
-		self.wert = wert
+    def __init__(self, wert):
+        self.wert = wert
 
-	def __toJSON__(self):
-		jsonstring = '{"wert":' + str(self.wert) + '}'
-		return jsonstring
+    def __to_json__(self):
+        jsonstring = '{"wert":' + str(self.wert) + '}'
+        return jsonstring
 
-# Klasse die Aufgerufen wird
+
 class Encoder:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def encode(self, obj: Datenhalter): 
-		return obj.__toJSON__()
-	
+    def encode(self, obj: Datenhalter):
+        return obj.__to_json__()
+
+
 # Main Methode
 if __name__ == '__main__':
-	# Serialisierer erstellen
-	encoder = Encoder()
-	# Datenhalter erstellen
-	datenhalter = Datenhalter(42)
-	# Datenhalter serialisieren
-	print(encoder.encode(datenhalter))
-
+    # Serialisierer erstellen
+    encoder = Encoder()
+    # Datenhalter erstellen
+    datenhalter = Datenhalter(42)
+    # Datenhalter serialisieren
+    print(encoder.encode(datenhalter))
