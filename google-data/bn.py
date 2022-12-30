63db0dec6645dc9bc69676f81f5c74105be2c84d
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2022-12-30 19:37:43 +0000

    random generator

--- google-data/bn.py
+++ google-data/bn.py
@@ -34,63 +34,65 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
+
 def extract_names(filename):
-  """
-  Given a file name for baby.html, returns a list starting with the year string
-  followed by the name-rank strings in alphabetical order.
-  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
-  """
-  # regex to find the year
-  year_re = re.compile(r'<h3 align="center">Popularity in (\d+)<\/h3>')
+    """
+    Given a file name for baby.html, returns a list starting with the year string
+    followed by the name-rank strings in alphabetical order.
+    ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+    """
+    # regex to find the year
+    year_re = re.compile(r'<h3 align="center">Popularity in (\d+)<\/h3>')
 
-  # regex to find the name and rank
-  name_re = re.compile(r'<tr align="right"><td>(\d+)<\/td><td>(\w+)<\/td><td>(\w+)<\/td>')
+    # regex to find the name and rank
+    name_re = re.compile(
+        r'<tr align="right"><td>(\d+)<\/td><td>(\w+)<\/td><td>(\w+)<\/td>')
 
-  # 1. Open and read file steam
-  with open(filename, "r") as file:
-    year = None
-    names = {}
-    for line in file:
-      if not year:
-        if result := year_re.search(line):
-          year = result[1]
-      else:
-        if result := name_re.search(line):
-          rank = result[1]
-          male = result[2]
-          female = result[3]
+    # 1. Open and read file steam
+    with open(filename, "r") as file:
+        year = None
+        names = {}
+        for line in file:
+            if not year:
+                if result := year_re.search(line):
+                    year = result[1]
+            else:
+                if result := name_re.search(line):
+                    rank = result[1]
+                    male = result[2]
+                    female = result[3]
 
-          for name in [male, female]:
-            names.setdefault(name, rank)
-          
+                    for name in [male, female]:
+                        names.setdefault(name, rank)
 
-  # 3. Get the names and rank in a dictionary and print it
-  # 4. Build a list and print it
-  return [year] + [f"{name} {rank}" for name, rank in sorted(names.items())]
+    # 3. Get the names and rank in a dictionary and print it
+    # 4. Build a list and print it
+    return [year] + [f"{name} {rank}" for name, rank in sorted(names.items())]
 
 
 def main():
-  # This command-line parsing code is provided.
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
+    # This command-line parsing code is provided.
+    # Make a list of command line arguments, omitting the [0] element
+    # which is the script itself.
+    args = sys.argv[1:]
 
-  if not args:
-    print("usage: [--summaryfile] file [file ...]")
-    sys.exit(1)
+    if not args:
+        print("usage: [--summaryfile] file [file ...]")
+        sys.exit(1)
 
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
+    # Notice the summary flag and remove it from args if it is present.
+    summary = False
+    if args[0] == '--summaryfile':
+        summary = True
+        del args[0]
 
-  # +++your code here+++
-  for arg in args:
-    names = extract_names(arg)
-    print(names)
-  # For each filename, get the names, then either print the text output
-  # or write it to a summary file
-  
+    # +++your code here+++
+    for arg in args:
+        names = extract_names(arg)
+        print(names)
+    # For each filename, get the names, then either print the text output
+    # or write it to a summary file
+
+
 if __name__ == '__main__':
-  main()
+    main()
